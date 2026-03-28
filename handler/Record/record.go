c19d0d71c06f3dbc358f9b0542a693cb115ff096
Blue-Onion <rawatadityasingh963@gmail.com>
2026-03-28 21:38:44 +0530

    Hello

--- handler/Record/record.go
+++ handler/Record/record.go
@@ -1,10 +1,28 @@
 package record
-func RecordEvent(){
 
-}
-func WriteEvent(){
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"os"
 
-}
-func ReadEvent(){
+	camera "github.com/Blue-Onion/MahilAi/handler/Camera"
+)
+func RecordEvent(event *camera.Event ){
 	
+
+}
+func WriteEvent(event *camera.Event){
+	file, err := os.OpenFile("events.log", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		log.Fatal(err)
+	}
+	//hilihih
+	defer file.Close()
+	log.SetOutput(file)
+	data,err:=json.Marshal(event)
+	if err!=nil{
+		fmt.Println(err.Error())
+	}
+	log.Println(string(data)+"\n")
 }
